6935f438737ba43836051ef11ed0ccea8f571081
John D. Ament <johndament@apache.org>
2017-07-08 09:51:33 -0400

    Handle resources found in the child JARs of a WAR as well as the contents of the war.  This closes #39

---
+++ impl/src/test/java/org/jboss/arquillian/container/weld/embedded/WebArchiveWithLibJarExtensionTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.container.weld.embedded;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.enterprise.inject.spi.Extension;
+
+@RunWith(Arquillian.class)
+public class WebArchiveWithLibJarExtensionTestCase {
+
+    @Deployment
+    public static Archive<?> createdeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+                .addAsServiceProvider(Extension.class, WasCalledExtension.class)
+                .addClasses(WasCalledExtension.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsLibraries(jar)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void shouldLoadExtensionDiscoveredInWebInfLibJar() throws Exception {
+        Assert.assertTrue("Extension should have been loaded", WasCalledExtension.wasCalled);
+    }
+}
