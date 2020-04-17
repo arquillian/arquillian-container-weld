2b297271ae3f13b3bf037a98bf4a6579349659cd
Scott M Stark <starksm64@gmail.com>
2020-04-17 07:43:53 -0500

    Update for changes in arquillian and update all jakarta apis
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- impl/src/main/java/org/jboss/arquillian/container/weld/embedded/mock/MockTransactionServices.java
+++ impl/src/main/java/org/jboss/arquillian/container/weld/embedded/mock/MockTransactionServices.java
@@ -17,13 +17,13 @@
 
 package org.jboss.arquillian.container.weld.embedded.mock;
 
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.Synchronization;
-import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
+import jakarta.transaction.HeuristicMixedException;
+import jakarta.transaction.HeuristicRollbackException;
+import jakarta.transaction.NotSupportedException;
+import jakarta.transaction.RollbackException;
+import jakarta.transaction.Synchronization;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
 
 import org.jboss.weld.transaction.spi.TransactionServices;
 
